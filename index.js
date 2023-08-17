690425832885e6b40c5d4c9dd1823a0fd279e396
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2023-08-16 21:21:12 -0500

    escribi el for que recorre el array de avatares en la api

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 import rik from "./module.mjs";
 
-  var root = document.getElementById("root");
-  var container = document.createElement("p")
-  container.innerText = rik.results[11].name;
-  root.appendChild(container)
+    var root = document.getElementById("root");
+
+    for (var i = 0; rik.results.length; i++){
+      var avatar = document.createElement("img")
+      avatar.src = rik.results[i].image;
+      root.appendChild(avatar)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   console.log()
+
+var box = document.createElement('div');
+  box.className= 'box';
+  root.appendChild(box);
+
+
+
+
+
+  // --------------------------------------------------------------------
+
+  document.getElementById("search").addEventListener('click', function () {
+    var input = document.getElementById("search").value;
+  })
 
--- module.mjs
+++ module.mjs
@@ -1,6 +1,6 @@
 
 const url1 = 'https://jsonplaceholder.typicode.com/posts/1/comments';
-const url2 = 'https://rickandmortyapi.com/api/location';
+const url2 = 'https://rickandmortyapi.com/api/character';
 
 let response = await fetch(url2)
 let rik = await response.json(response)
