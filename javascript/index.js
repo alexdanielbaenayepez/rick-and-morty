fac0a12cf6912b101c16c6ff7dd758dc836919c0
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2023-08-30 07:42:30 -0500

    agregue el texto debajo del logo, el evinto onchange tiene que ver con la actualizacion de los datos cuando hago click en el boton enviar

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,3 @@
-/*
-*En esta app traigo datos de una API y creo una tarjeta dinámica que se actualiza con los datos de la API
-de rik and morty, la página debe tener un menú de navegación con un buscador y el cuerpo de la página cargar
-las tarjetas con el contenido de la API
-* */
-
 import rik from "./module.mjs";
 
     var root = document.getElementById("root");
@@ -30,7 +24,7 @@ import rik from "./module.mjs";
           status.innerText = "Status: " + rik.results[i].status;
 
       var descripcion = document.createElement('p')
-          descripcion.innerText = "Type: " + rik.results[i].type;
+          descripcion.innerText = 'Type: ' + rik.results[i].type;
 
       box.appendChild(containerImagen)
       box.appendChild(hr)
@@ -44,14 +38,11 @@ import rik from "./module.mjs";
 
   //------------------------------------------------------------------------
 
-
-
-
-  // --------------------------------------------------------------------
-
-  document.getElementById("submit").addEventListener('click', function () {
+  document.getElementById("submit").addEventListener('keyup', function (e, root) {
     var input = document.getElementById("search").value;
-    console.log(input)
     root.innerHTML(input)
+
+    //el evento onchonge tiene que ver con la actualizacion de los datos de entrada en el input
+    // https://es.javascript.info/events-change-input
   })
 
